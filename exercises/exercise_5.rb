7742da1bb17ee1c0ffcbb63b0d15a5f787bfae84
Gabriel Silva <j.gabriel.888@gmail.com>
2021-04-26 22:46:46 +0000

    exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,17 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_annual = Store.sum(:annual_revenue)
+
+puts "***********************"
+puts "Total annual is: #{@total_annual}"
+
+@average_annual = Store.average(:annual_revenue)
+puts "***********************"
+puts "Average annual is: #{@average_annual}"
+
+@stores_over_milion = Store.where("annual_revenue > 1000000")
+# binding.pry debuging
+puts "***********************"
+puts "Milionare: #{@stores_over_milion[0].name}"
