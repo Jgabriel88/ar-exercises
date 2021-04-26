7222a8e76f5ca392ba98f6af3bd2b1269d79c4d0
Gabriel Silva <j.gabriel.888@gmail.com>
2021-04-26 21:13:21 +0000

    exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,36 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+require_relative '../setup'
+
+puts "Exercise 1"
+puts "----------"
+
+# Your code goes below here ...
+
+class Store < ActiveRecord::Base
+  store1 = Store.new
+  store1.name = "Burnaby"
+  store1.annual_revenue = 300000
+  store1.mens_apparel = true
+  store1.womens_apparel = true
+  store1.save
+  
+  store2 = Store.new
+  store2.name = "Richmond"
+  store2.annual_revenue = 300000
+  store2.mens_apparel = true
+  store2.womens_apparel = true
+  store2.save
+
+  store3 = Store.new
+  store3.name = "Gastown"
+  store3.annual_revenue = 300000
+  store3.mens_apparel = true
+  store3.womens_apparel = true
+  store3.save
+ 
+end
+puts "***********************"
+
+puts Store.count
